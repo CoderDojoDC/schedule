c648be6be1db015f50a032c7c85c89c4a7c556fb
Frank Hunleth <fhunleth@troodon-software.com>
2016-12-08 20:55:02 -0500

    Update for 2017

--- schedule.py
+++ schedule.py
@@ -3,19 +3,19 @@ from datetime import date, timedelta
 import holidays
 
 ch = holidays.UnitedStates()
-ch.update({ "2016-02-07": "Super Bowl Sunday", "2016-02-14": "Valentine's Day",
-            "2016-03-27": "Easter Sunday", "2016-05-08": "Mother's Day",
-            "2016-06-19": "Father's Day", "2016-07-03": "4th of July Weekend",
-            "2016-10-02": "Rosh Hashanah (begins at sundown)", "2016-11-27":
+ch.update({ "2017-02-05": "Super Bowl Sunday", "2017-02-14": "Valentine's Day",
+            "2017-04-16": "Easter Sunday", "2017-05-14": "Mother's Day",
+            "2017-06-18": "Father's Day", "2017-07-02": "4th of July Weekend",
+            "2017-09-22": "Rosh Hashanah (begins at sundown)", "2017-11-26":
             "Thanksgiving Weekend"})
 
 def secondsundays(d):
-    while d.year == 2016:
+    while d.year == 2017:
         yield d
         d += timedelta(days = 14)
 
-start1 = date(2016, 1, 3)
-start2 = date(2016, 1, 10)
+start1 = date(2017, 1, 8)
+start2 = date(2017, 1, 15)
 schedule1 = [d.strftime('%Y-%m-%d') for d in secondsundays(start1)]
 schedule2 = [d.strftime('%Y-%m-%d') for d in secondsundays(start2)]
 
